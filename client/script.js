43baab1aa49d489cdc131a3c36cc6f89c5f18d5f
Tajumulla <tajju3069@gmail.com>
2023-01-05 19:05:57 +0530

    update contract

--- client/script.js
+++ client/script.js
@@ -70,7 +70,7 @@ const handleSubmit = async(e) => {
   loader(messageDiv);
 
   // fetch data from server
-  const response = await fetch('http://localhost:5000/', {
+  const response = await fetch('https://codex-openai-tiv3.onrender.com', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
